3b17505cfea29e2d9735d4d7e2839d94127d58e8
MAHESH837492 <kmrkrk93817@gmail.com>
2025-08-04 17:00:18 +0530

    add.name

--- test2.py
+++ test2.py
@@ -1,5 +1,10 @@
 # nkufjgkjbuybkjhjbhb
-a= int(input("Enter a: "))
-b= int(input("Enter b: "))
-result= a / b
+# a= int(input("Enter a: "))
+# b= int(input("Enter b: "))
+# result= a / b
+# print(result)
+
+a= input("Mahesh")
+b= input("kurva")
+result= b+a
 print(result)
